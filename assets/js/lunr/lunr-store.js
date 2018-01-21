aecb0f6852d54d41820cca0f5cbf8c786b149966
James Mensch <james@jamesmensch.com>
2018-01-21 17:20:42 -0600

    Site updated at 2018-01-21 23:20:42 UTC

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,51 @@
+var store = [{
+        "title": "Node.js GraphQL & PostgreSQL Quickstart",
+        "excerpt":"View the source code hereGraphQL is an API query language that aims to replace REST. With GraphQL, you cancombine data from multiple sources into one response and expose it as a single API endpoint. We’ll explore one option in this below: querying data from PostgreSQL.PrerequisitesFrom your terminal, install the necessary...","categories": ["tutorials"],
+        "tags": ["graphql","postgresql","javascript","node","nodejs"],
+        "url": "/tutorials/graphql-and-postgresql-node-quickstart/",
+        "teaser":null},{
+        "title": "Database source control, versioning, and environment management with Sqitch",
+        "excerpt":"View the source code hereWe’ve been using source control for application code for years. Even infrastructure can be coded and versioned controlled. But some engineers are still oblivious to the need for version control around databases that house crucial data their applications rely on. Crazy, right? I use Sqitch, which...","categories": ["tutorials"],
+        "tags": ["database","postgresql","sqitch"],
+        "url": "/tutorials/database-source-control-sqitch/",
+        "teaser":null},{
+        "title": "Using User-centric Feedback Loops to Deliver a Better Product, Faster.",
+        "excerpt":"Building with feature scope minimality in mind is the bread and butter of startup success. You want to stay agile, pivot often and without accruing too much technical debt, and verify your idea as early as possible. Getting the user involved in your product feedback loop process early will accelerate...","categories": ["thoughts"],
+        "tags": ["project-management"],
+        "url": "/thoughts/feedback-loops/",
+        "teaser":null},{
+        "title": "My holidays 2017 Reading List",
+        "excerpt":"My immediate post-college self chose what I read with the mentality “if this doesn’t level me up professionally, it isn’t worth my time.” I became knowledgeable in everything from mindfulness techniques to product management. Unfortunately, it turns out cutting out all of the leisurely content from your life turns you...","categories": ["thoughts"],
+        "tags": ["reading-list"],
+        "url": "/thoughts/reading-list-holidays-2017/",
+        "teaser":null},{
+        "title": "Managing Willpower and Staying Productive",
+        "excerpt":"Wikipedia defines ego depletions as “idea that self-control or willpower draws upon a limited pool of mental resources that can be used up. When the energy for mental activity is low, self-control is typically impaired, which would be considered a state of ego depletion”. I usually call it the “afternoon...","categories": ["thoughts"],
+        "tags": ["motivation","productivity"],
+        "url": "/thoughts/managing-willpower/",
+        "teaser":null},{
+        "title": "Production Apollo GraphQL - Essential Setup and Tooling",
+        "excerpt":"GraphQL has revolutionized the way we think about APIs, and Apollo has provided a suite of frameworks to get up and running quick and easy. We’ve been using GraphQL and Apollo since inception at the The Wed Clique. Since then I’ve learned the ins and outs of the Apollo GraphQL...","categories": ["tutorials"],
+        "tags": ["graphql","production","javascript","node","nodejs"],
+        "url": "/tutorials/production-graphql-tooling-1/",
+        "teaser":null},{
+        "title": "The Perfect Daily Standup",
+        "excerpt":"Regardless of management style or company size, the daily standup has become ubiquitous as an essential tool for every self-organizing team. I’ve both participated in and led standups for engineering teams and cross business units, and formed my own opinion on what makes the most effective standup, which I’ve detailed...","categories": ["thoughts"],
+        "tags": ["productivity","management"],
+        "url": "/thoughts/perfect-daily-standup/",
+        "teaser":null},{
+        "title": "Production Apollo GraphQL - Authentication & Authorization",
+        "excerpt":"NOTE: This example will be using Apollo-Client 1.x and Apollo-Server-Express 1.2.0.This is part two of my Production GraphQL series. Check out part 1, “Essential Setup and Tooling”, here.Each field provided by a GraphQL Schema corresponds to a resolver that produces the correct field. This gives us unparalleled control over our...","categories": ["tutorials"],
+        "tags": ["graphql","production","javascript","node","nodejs"],
+        "url": "/tutorials/production-graphql-2-auth/",
+        "teaser":null},{
+        "title": "Reflections on 2017",
+        "excerpt":"2017 was a year of change. I quit my job, became an entrepreneur, moved from Philadelphia to Austin, overcame challenges and failed at others.I’ve learned more in the last 12 months than the previous 25 years combined. This is one of my reflections, made public in that hopes that others...","categories": ["thoughts"],
+        "tags": [],
+        "url": "/thoughts/2017-reflection/",
+        "teaser":null},{
+        "title": "My 2018 Q1 Reading List",
+        "excerpt":"Committing to 25 books this year, so that means Q1 2018 should have 6. Here’s what made the cut! Between the World and Me - Ta-Nehisi Coates Data Science for Business: What You Need to Know about Data Mining and Data-Analytic Thinking The Manager’s Path: A Guide for Tech Leaders...","categories": ["thoughts"],
+        "tags": ["reading-list"],
+        "url": "/thoughts/2018-q1-reading-list/",
+        "teaser":null}]
